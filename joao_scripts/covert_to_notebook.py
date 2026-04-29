5cbea9292e76e389c5089d3d859bb7ab4d855439
joao bazzo <joao.bazzo@gmail.com>
2026-04-28 20:21:28 -0400

    add reprojection method

---
+++ joao_scripts/covert_to_notebook.py
@@ -0,0 +1,10 @@
+# Save this as convert_to_notebook.py and run it once
+import json, textwrap
+
+# ... or just use the nbformat package:
+# pip install nbformat
+import nbformat as nbf
+
+nb = nbf.v4.new_notebook()
+
+cells = []
